4fe755beffc079f74b12911070d3165de0fd8e42
𝕊𝔸ℍ𝕆𝕆ℕ ℍ𝕌𝕊𝕊𝔸𝕀ℕ 𝔸ℕ𝕊𝔸ℝ𝕀 <162339117+Sahoon1@users.noreply.github.com>
2025-02-21 20:48:22 +0500

    work

---
+++ Dart language/index.dart
@@ -0,0 +1,19 @@
+void main(){
+  print("hello dart language");
+  // print is a bultin function to print anything on screen
+  //data type
+  int num = 60; //integer
+  int num1 = 50; //integer
+  double double_num = 10.5 ; // double/float
+  bool bool_val = true ; // boolean - only has two values:true or false
+  var var_name = 'learning dart';
+
+
+  // operators
+  // arithmetic operator
+  print(num + num1); // addition
+  print(num - num1); //subtaction
+  print(num * num); // mutiplication
+  print(num / num1); // division
+  print(num % num1); // modulass
+}
